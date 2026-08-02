159a5a290d8f51b5953648dd7c99a390403c63a0
Shweta Chahar <shwetachahar842@gmail.com>
2026-08-02 23:45:53 +0530

    Add solutions for Assisted and Self problems (Java)

---
+++ Assisted-Problems/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.println("Sum: " + (a + b));
+        sc.close();
+    }
+}
